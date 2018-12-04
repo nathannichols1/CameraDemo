7190f61abb84e9229176b122b8f2ce07750ab6fb
NATHAN NICHOLS <rl235817@rhs-macair-40.local>
2018-12-04 10:35:14 -0600

    Initial Commit

---
+++ CameraDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraDemo
+//
+//  Created by NATHAN NICHOLS on 12/4/18.
+//  Copyright © 2018 NATHAN NICHOLS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
